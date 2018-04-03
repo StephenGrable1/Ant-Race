958dc7631fd129aa20f9cc32ef50c0a62ee67c5a
StephenGrable1 <stephengrable@gmail.com>
2018-04-03 19:38:03 -0400

    pull data from api and put into seperate file for fallback purposes

---
+++ client/src/data.js
@@ -0,0 +1,36 @@
+const AntAPIdata = {
+    "data": {
+      "ants": [
+        {
+          "name": "Marie ‘Ant’oinette",
+          "color": "BLACK",
+          "length": 12,
+          "weight": 2
+        },
+        {
+          "name": "Flamin’ Pincers",
+          "color": "RED",
+          "length": 11,
+          "weight": 2
+        },
+        {
+          "name": "Big Susan",
+          "color": "BLACK",
+          "length": 20,
+          "weight": 5
+        },
+        {
+          "name": "The Unbeareable Lightness of Being",
+          "color": "SILVER",
+          "length": 5,
+          "weight": 1
+        },
+        {
+          "name": "‘The Duke’",
+          "color": "RED",
+          "length": 17,
+          "weight": 3
+        }
+      ]
+    }
+  }
